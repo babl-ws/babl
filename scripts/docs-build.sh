49e378c405f0ba096cd2eaaef1388bc59b6078ca
epickrram <mark@aitusoftware.com>
2020-06-03 19:54:41 +0100

    Fix sed expression in docs build script.

--- scripts/docs-build.sh
+++ scripts/docs-build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+set -x
 PROJECT_DIR="$(dirname $0)/.."
 cd "$PROJECT_DIR" || exit
 ./gradlew javadoc
@@ -24,13 +24,17 @@ cp -R "$PROJECT_DIR/doc/docs" "$OUTPUT_DIR/"
 mkdir "$OUTPUT_DIR/docs/static"
 cp -R "$PROJECT_DIR/build/docs/javadoc" "$OUTPUT_DIR/docs/static/"
 cd "$OUTPUT_DIR/docs" && mkdir themes &&  cd themes && git clone https://github.com/babl-ws/book.git
-sed -ie 's/max-width: 800px/max-width: 1000px' book/sass/_content.scss
+sed -ie 's/max-width: 800px/max-width: 1000px/g' book/sass/_content.scss
 find "$OUTPUT_DIR/docs/content" -name '*.md' | xargs sed -ie "s/BABL_VERSION/${VERSION}/g"
 cd "$OUTPUT_DIR/docs/" && $ZOLA_BIN build --drafts
 echo "$OUTPUT_DIR"
 if [[ "x$1" == "xserve" ]]; then
   $ZOLA_BIN serve --drafts # || rm -r "$OUTPUT_DIR"
 elif [[ "x$1" == "xpublish" ]]; then
+  if [[ "x$2" == "x" ]]; then
+    echo "Supply the publish directory"
+    exit 1
+  fi
   PUBLISH_DIR="$2"
   echo "Publishing to $PUBLISH_DIR"
   rsync -a "$OUTPUT_DIR/docs/public/" "$PUBLISH_DIR/"
