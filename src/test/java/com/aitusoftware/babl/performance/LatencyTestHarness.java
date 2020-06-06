3473e38c62a1297040efcc72d6dc73292389d52d
epickrram <mark@aitusoftware.com>
2020-06-06 15:26:19 +0100

    Improve latency test harness

--- src/test/java/com/aitusoftware/babl/performance/LatencyTestHarness.java
+++ src/test/java/com/aitusoftware/babl/performance/LatencyTestHarness.java
@@ -17,8 +17,6 @@
  */
 package com.aitusoftware.babl.performance;
 
-import static com.google.common.truth.Truth.assertThat;
-
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.URL;
@@ -41,8 +39,11 @@ import org.agrona.DirectBuffer;
 import org.agrona.MutableDirectBuffer;
 import org.agrona.concurrent.UnsafeBuffer;
 
+@SuppressWarnings("ForLoopReplaceableByForEach")
 final class LatencyTestHarness
 {
+    private static final AtomicInteger GENERATOR_ID = new AtomicInteger();
+
     private final int clientCount;
     private final int threadCount;
     private final int messagesPerSecond;
@@ -50,6 +51,7 @@ final class LatencyTestHarness
     private final int warmUpIterations;
     private final int messagesPerIteration;
     private final int payloadSize;
+    private final Config config;
 
     LatencyTestHarness(
         final int clientCount, final int threadCount, final int messagesPerSecond,
@@ -63,12 +65,7 @@ final class LatencyTestHarness
         this.warmUpIterations = warmUpIterations;
         this.messagesPerIteration = messagesPerIteration;
         this.payloadSize = payloadSize;
-    }
-
-    public static void main(final String[] args) throws Exception
-    {
-        new LatencyTestHarness(16, 2, 160000, 4000, 10, 1000, 200)
-            .runLatencyTest(new InetSocketAddress("localhost", 8080));
+        config = new Config(clientCount, threadCount, messagesPerSecond);
     }
 
     Histogram runLatencyTest(final InetSocketAddress serverAddress)
@@ -79,34 +76,50 @@ final class LatencyTestHarness
         final long expectedRuntimeNs = (warmUpIterations + measurementIterations) *
             (messagesPerIteration * interMessageInterval);
         final List<List<ClientAndHandler>> clientSets =
-            distributeClients(serverAddress, clientCount, threadCount, interMessageInterval);
+            distributeClients(serverAddress, config);
+        logConfig(messagesPerSecond, expectedRuntimeNs,
+            warmUpIterations, measurementIterations, messagesPerIteration, config);
         final Histogram allResults = latencyHistogram();
         final List<Future<Histogram>> histogramResults = new ArrayList<>();
-        for (int i = 0; i < threadCount; i++)
+        try
         {
-            final List<ClientAndHandler> clientSet = clientSets.get(i);
-            final Future<Histogram> measurementResult = executeLoad(threadPool, warmUpIterations,
-                messagesPerIteration, measurementIterations, payloadSize, interMessageInterval, clientSet);
-            histogramResults.add(measurementResult);
+            for (int i = 0; i < threadCount; i++)
+            {
+                final List<ClientAndHandler> clientSet = clientSets.get(i);
+                final Future<Histogram> measurementResult = executeLoad(
+                    threadPool, warmUpIterations, messagesPerIteration, measurementIterations,
+                    payloadSize, interMessageInterval, clientSet, clientCount);
+                histogramResults.add(measurementResult);
+            }
+            for (final Future<Histogram> histogramResult : histogramResults)
+            {
+                final Histogram histogram = histogramResult.get(5 * expectedRuntimeNs, TimeUnit.NANOSECONDS);
+                allResults.add(histogram);
+            }
+            if (allResults.getTotalCount() < 0.9f * measurementIterations * messagesPerIteration)
+            {
+                throw new IllegalStateException(String.format("Unexpected total number of results: %d",
+                    allResults.getTotalCount()));
+            }
         }
-        for (final Future<Histogram> histogramResult : histogramResults)
+        finally
         {
-            final Histogram histogram = histogramResult.get(20 * expectedRuntimeNs, TimeUnit.NANOSECONDS);
-            allResults.add(histogram);
+            threadPool.shutdown();
+            if (!threadPool.awaitTermination(5 * expectedRuntimeNs, TimeUnit.NANOSECONDS))
+            {
+                System.err.println("WARN: Threads did not stop");
+            }
         }
-        threadPool.shutdown();
-        assertThat(threadPool.awaitTermination(20 * expectedRuntimeNs, TimeUnit.NANOSECONDS)).isTrue();
         allResults.outputPercentileDistribution(System.out, 1d);
         return allResults;
     }
-
-    private static final AtomicInteger GENERATOR_ID = new AtomicInteger();
 
     private Future<Histogram> executeLoad(
         final ExecutorService threadPool, final int warmupIterations,
         final int messagesPerIteration, final int measurementIterations,
         final int payloadSize, final long interMessageInterval,
-        final List<ClientAndHandler> clientSet)
+        final List<ClientAndHandler> clientSet,
+        final int totalClientCount)
     {
         return threadPool.submit(() ->
         {
@@ -117,14 +130,18 @@ final class LatencyTestHarness
                 final MutableDirectBuffer buffer = new UnsafeBuffer(new byte[payloadSize]);
                 deliverMessageBatches(resultHistogram, clientSet,
                     warmupIterations, messagesPerIteration,
-                    payloadSize, buffer, interMessageInterval);
+                    payloadSize, buffer, interMessageInterval, totalClientCount);
                 resultHistogram.reset();
-                System.out.printf("%s warmup complete%n", Thread.currentThread().getName());
+                System.out.printf("[%s] warmup complete%n", Thread.currentThread().getName());
                 System.out.flush();
-                deliverMessageBatches(resultHistogram, clientSet,
+                final long backPressureCount = deliverMessageBatches(resultHistogram, clientSet,
                     measurementIterations, messagesPerIteration,
-                    payloadSize, buffer, interMessageInterval);
-
+                    payloadSize, buffer, interMessageInterval, totalClientCount);
+                if (backPressureCount != 0)
+                {
+                    System.out.printf("[%s] Back-pressure count: %d%n",
+                        Thread.currentThread().getName(), backPressureCount);
+                }
                 return resultHistogram;
             }
             catch (final Exception e)
@@ -140,43 +157,96 @@ final class LatencyTestHarness
         return new Histogram(10_000_000, 3);
     }
 
-    private List<List<ClientAndHandler>> distributeClients(
-        final InetSocketAddress serverAddress, final int clientCount,
-        final int clientThreadCount, final long expectedIntervalNs) throws IOException
+    private static void logConfig(
+        final long messagesPerSecond,
+        final long expectedRuntimeNs,
+        final int warmUpIterations,
+        final int measurementIterations,
+        final int messagesPerIteration,
+        final Config config)
+    {
+        System.out.printf("Using %d threads, %d clients per-thread%n", config.threadCount, config.perThreadClientCount);
+        System.out.printf("%d warm-up iterations, %d measurement iterations%n",
+            warmUpIterations, measurementIterations);
+        System.out.printf("Aggregate throughput %d msg/sec%n", messagesPerSecond);
+        System.out.printf("Per-thread batch internal %dus%n", TimeUnit.NANOSECONDS.toMicros(
+            config.perThreadBatchIntervalNs));
+        System.out.printf("Expected runtime %ds%n", TimeUnit.NANOSECONDS.toSeconds(expectedRuntimeNs));
+        System.out.printf("Expecting ~%d total measurement results%n", measurementIterations * messagesPerIteration);
+    }
+
+    private static List<List<ClientAndHandler>> distributeClients(
+        final InetSocketAddress serverAddress,
+        final Config config) throws IOException
     {
         final List<List<ClientAndHandler>> clientSets = new ArrayList<>();
-        for (int i = 0; i < clientThreadCount; i++)
+        for (int i = 0; i < config.threadCount; i++)
         {
             clientSets.add(new ArrayList<>());
         }
         final URL url = new URL(String.format("http://%s:%s/foo",
             serverAddress.getHostString(), serverAddress.getPort()));
 
-        for (int i = 0; i < clientCount; i++)
+        for (int i = 0; i < config.clientCount; i++)
         {
             final Histogram histogram = latencyHistogram();
             final MeasuringClientEventHandler eventHandler = new MeasuringClientEventHandler(histogram,
-                expectedIntervalNs);
+                config.perThreadBatchIntervalNs);
             final Client client = new Client(eventHandler);
             client.connect(url);
-            assertThat(client.upgradeConnection()).isTrue();
-            clientSets.get(i % clientThreadCount).add(new ClientAndHandler(client, eventHandler));
+            if (!client.upgradeConnection())
+            {
+                throw new IllegalStateException("Failed to upgrade connection");
+            }
+            clientSets.get(i % config.threadCount).add(new ClientAndHandler(client, eventHandler));
         }
         return clientSets;
     }
 
-    private void deliverMessageBatches(
+    private static final class Config
+    {
+        private final long perThreadClientCount;
+        private final long perThreadBatchIntervalNs;
+        private final int threadCount;
+        private final int clientCount;
+
+        private Config(
+            final int clientCount,
+            final int threadCount,
+            final int messagesPerSecond)
+        {
+            if (clientCount % threadCount != 0)
+            {
+                throw new IllegalArgumentException("Client count must be a multiple of thread count");
+            }
+            if (clientCount < threadCount)
+            {
+                throw new IllegalArgumentException("Client count must be >= thread count");
+            }
+            final long globalInterMessageIntervalNs = TimeUnit.SECONDS.toNanos(1) / messagesPerSecond;
+            perThreadClientCount = clientCount / threadCount;
+            perThreadBatchIntervalNs = globalInterMessageIntervalNs * clientCount;
+            this.threadCount = threadCount;
+            this.clientCount = clientCount;
+        }
+    }
+
+    private static long deliverMessageBatches(
         final Histogram resultHistogram,
         final List<ClientAndHandler> clients,
         final int iterations, final int messagesPerIteration,
         final int payloadSize, final MutableDirectBuffer buffer,
-        final long interMessageInterval) throws IOException
+        final long interMessageInterval,
+        final int totalClientCount) throws IOException
     {
+        final long interBatchInterval = interMessageInterval * totalClientCount;
+        long backPressureCount = 0;
         int remainingIterations = iterations;
         while (remainingIterations-- != 0)
         {
-            int remainingMessages = messagesPerIteration;
-            while (remainingMessages-- != 0)
+            final int totalBatchCount = messagesPerIteration / totalClientCount;
+            int remainingBatches = totalBatchCount;
+            while (remainingBatches-- != 0)
             {
                 final long startTime = System.nanoTime();
                 for (int clientIndex = 0; clientIndex < clients.size(); clientIndex++)
@@ -188,9 +258,10 @@ final class LatencyTestHarness
                     while (!client.offer(buffer, 0, payloadSize, ContentType.BINARY))
                     {
                         client.doWork();
+                        backPressureCount++;
                     }
                 }
-                final long endOfSendTime = startTime + interMessageInterval - 60L;
+                final long endOfSendTime = startTime + interBatchInterval - 60L;
                 while (System.nanoTime() < endOfSendTime)
                 {
                     for (int clientIndex = 0; clientIndex < clients.size(); clientIndex++)
@@ -198,7 +269,6 @@ final class LatencyTestHarness
                         clients.get(clientIndex).client.doWork();
                     }
                 }
-
             }
             while (true)
             {
@@ -206,7 +276,7 @@ final class LatencyTestHarness
                 for (int clientIndex = 0; clientIndex < clients.size(); clientIndex++)
                 {
                     clients.get(clientIndex).client.doWork();
-                    if (clients.get(clientIndex).handler.histogram.getTotalCount() < messagesPerIteration)
+                    if (clients.get(clientIndex).handler.histogram.getTotalCount() < totalBatchCount)
                     {
                         allComplete = false;
                         break;
@@ -223,6 +293,7 @@ final class LatencyTestHarness
                 clients.get(clientIndex).handler.histogram.reset();
             }
         }
+        return backPressureCount;
     }
 
     private static final class ClientAndHandler
@@ -258,10 +329,11 @@ final class LatencyTestHarness
             final ContentType contentType)
         {
             final long timestampNs = buffer.getLong(0);
-            final long rttNanos = System.nanoTime() - timestampNs;
+            final long currentNanos = System.nanoTime();
+            final long rttNanos = currentNanos - timestampNs;
             if (rttNanos < 0)
             {
-                throw new IllegalStateException();
+                throw new IllegalStateException(String.format("Start time %d < now %d", timestampNs, currentNanos));
             }
             else
             {
